f4a88786b80896ff74dff71eaceb576ecf2da332
Ibrahim Lahribli <dt.lahribli.ibrahim@gmail.com>
2019-11-28 14:31:44 +0100

    refactor:sideBar (style dropDownMenu component)

--- src/components/dropdownMenu.js
+++ src/components/dropdownMenu.js
@@ -5,12 +5,12 @@ import './Dropdown.css';
 function DropdownMenu(props) {
     return (
             <Row className="dropdown_menu" >
-              <Col className="dropdown_menu_label" lg={8} md={8} sm={8} xs={8}>
-                <h4 className="selected_team">{props.team}</h4>
+              <Col className="dropdown_menu__label" lg={8} md={8} sm={8} xs={8}>
+                <h4 className="team_label">{props.team}</h4>
                 <span className="team_count">{`${props.teamCount} members`}</span>
               </Col>
-              <Col className="dropdown_menu_icon" lg="auto" md="auto" sm="auto" xs="auto" >
-                 <i className="fa fa-fw fa-home" style={{ fontSize: '1em', marginTop:'100%', cursor:'pointer' }} />
+              <Col lg="auto" md="auto" sm="auto" xs="auto" >
+                 <i className="fa fa-fw fa-home dropdown_menu__icon"/>
               </Col>
             </Row>
     )
